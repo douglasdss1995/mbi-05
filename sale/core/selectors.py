4cf22cc17242e8a8b0b0f5aa6e67fa04dfd6ce5c
adeilsomelo <adeilso.a.z@gmail.com>
2026-02-12 11:35:33 -0400

    aula 10 - 12/02/2026(quinta-feira)

---
+++ sale/core/selectors.py
@@ -0,0 +1,47 @@
+from core import models
+
+
+def get_all_products():
+    return models.Product.objects.all()
+
+
+def get_all_employees():
+    return models.Employee.objects.all()
+
+
+def get_all_orders():
+    return models.Customer.objects.all()
+
+
+def get_product_by_id(product_id: int):
+    return models.Product.objects.get(id=product_id)
+
+
+def get_product_by_name(name: str):
+    return models.Product.objects.get(name=name)
+
+
+def get_employee_by_name(name: str):
+    return models.Employee.objects.get(name=name)
+
+
+def getcustomer_by_name(name: str):
+    return models.Customer.objects.get(name=name)
+
+
+def create_zone():
+    models.Zone.objects.create(name="Zone 1")
+
+
+def create_product():
+    product_group = models.ProductGroup.objects.get(id=1)
+    supplier = models.Supplier.objects.get(id=1)
+
+    return models.Product.objects.create(
+        name='Produto 01',
+        cost_price=1,
+        sale_price=2,
+        id_supplier=supplier,
+        id_product_group=product_group,
+        active=True
+    )
